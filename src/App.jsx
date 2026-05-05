888db90a46223899a860fa7ca983d6361a519705
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-05-05 07:49:29 +0530

    adding routing element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 const App = ()=>{
   return(
-    <div>
-      App component
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+                    <Route path="/dashboard" element={
+                        <>
+                            <SignedIn><Dashboard /></SignedIn>
+                            <SignedOut><RedirectToSignIn /></SignedOut>
+                        </>
+                    } />
+                    <Route path="/upload" element={
+                        <>
+                            <SignedIn><Upload /></SignedIn>
+                            <SignedOut><RedirectToSignIn /></SignedOut>
+                        </>
+                    } />
+                    <Route path="/my-files" element={
+                        <>
+                            <SignedIn><MyFiles /></SignedIn>
+                            <SignedOut><RedirectToSignIn /></SignedOut>
+                        </>
+                    } />
+                    <Route path="/subscriptions" element={
+                        <>
+                            <SignedIn><Subscription /></SignedIn>
+                            <SignedOut><RedirectToSignIn /></SignedOut>
+                        </>
+                    } />
+                    <Route path="/transactions" element={
+                        <>
+                            <SignedIn><Transactions /></SignedIn>
+                            <SignedOut><RedirectToSignIn /></SignedOut>
+                        </>
+                    } />
+                    <Route path="file/:fileId" element={
+                        <>
+                            <PublicFileView />
+                        </>
+                    }/>
+                    <Route path="/*" element={<RedirectToSignIn />} />
+
+     
+    </Routes>
+    </BrowserRouter>
   )
 }
 
