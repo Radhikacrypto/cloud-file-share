770c5a27ce7f8a3adea0e9ba33f191f8b75b2a11
Radhika Bacharwar <radhikabacharwar20@gmail.com>
2026-05-06 07:12:27 +0530

    adding functionnalitie

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Upload from "./pages/Upload.jsx";
+import MyFiles from "./pages/MyFiles.jsx";
+import Subscription from "./pages/Subscription.jsx";
+import Transactions from "./pages/Transactions.jsx";
 
-const App = ()=>{
-  return(
+const App = () => {
+  return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing />} />
-                    <Route path="/dashboard" element={
-                        <>
-                            <SignedIn><Dashboard /></SignedIn>
-                            <SignedOut><RedirectToSignIn /></SignedOut>
-                        </>
-                    } />
-                    <Route path="/upload" element={
-                        <>
-                            <SignedIn><Upload /></SignedIn>
-                            <SignedOut><RedirectToSignIn /></SignedOut>
-                        </>
-                    } />
-                    <Route path="/my-files" element={
-                        <>
-                            <SignedIn><MyFiles /></SignedIn>
-                            <SignedOut><RedirectToSignIn /></SignedOut>
-                        </>
-                    } />
-                    <Route path="/subscriptions" element={
-                        <>
-                            <SignedIn><Subscription /></SignedIn>
-                            <SignedOut><RedirectToSignIn /></SignedOut>
-                        </>
-                    } />
-                    <Route path="/transactions" element={
-                        <>
-                            <SignedIn><Transactions /></SignedIn>
-                            <SignedOut><RedirectToSignIn /></SignedOut>
-                        </>
-                    } />
-                    <Route path="file/:fileId" element={
-                        <>
-                            <PublicFileView />
-                        </>
-                    }/>
-                    <Route path="/*" element={<RedirectToSignIn />} />
-
-     
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/my-files" element={<MyFiles />} />
+        <Route path="/subscriptions" element={<Subscription />} />
+        <Route path="/transactions" element={<Transactions />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
